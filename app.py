a5f7ae7bd3921050bb604ec770025c61db67b852
qptechadmin <qpadmin@quickproptech.com>
2024-02-08 16:18:56 +0530

    fix pnl

--- app.py
+++ app.py
@@ -16,12 +16,12 @@ import threading
 import time
 
 class User: 
-    def __init__(self, id, username, password):
+    def _init_(self, id, username, password):
         self.id = id
         self.username = username
         self.password = password
 
-    def __repr__(self):
+    def _repr_(self):
         return f'<User: {self.username}>'
 
 users = []
@@ -30,7 +30,7 @@ users.append(User(id=2, username='Becca', password='secret'))
 users.append(User(id=3, username='Carlos', password='somethingsimple'))
 
 
-app = Flask(__name__)
+app = Flask(_name_)
 app.secret_key = 'fnyhwrbc1fyfulg3opt6pkj25nagxphi'
 
 # Replace with your actual API key and access token
@@ -121,7 +121,7 @@ def get_last_traded_price(stock_symbol):
         return None
 
 
-def get_p_n_l():
+def p_n_l():
 
     # Replace with the actual API endpoint provided by Zerodha for last pnl price
     api_url = f"https://api.kite.trade/portfolio/positions"
@@ -142,25 +142,47 @@ def get_p_n_l():
     else:
         return None
 
+def quantity():
+
+    # Replace with the actual API endpoint provided by Zerodha for last pnl price
+    api_url = f"https://api.kite.trade/portfolio/positions"
+
+    # Include your API key in the headers
+    headers = {
+        "X-Kite-Version": "3",
+        "Authorization": f"token {api_key}:{access_token}"
+    }
+
+    # Make an API request
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        quantity = data["net"]["quantity"]
+        return quantity
+    else:
+        return None
+
 @app.route('/get_last_traded_price_and_profit_loss')
 def get_last_traded_price_and_profit_loss():
     stock_symbol = request.args.get('symbol')
     last_traded_price = get_last_traded_price(stock_symbol)
-    profit_loss = get_p_n_l
+    profit_loss = p_n_l
+    quantity = quantity
 
     # Fetch the average price from the position details
     position = next((p for p in position_details if p['symbol'] == stock_symbol), None)
     if position:
         average_price = position['average_price']
-        quantity = position['quantity']
+        #quantity = position['quantity']
 
         # Calculate profit/loss and change percentage
-        #if position['type'] == 'Buy':
-        #    profit_loss = (last_traded_price - average_price) * quantity
-        #elif position['type'] == 'Sell':
-        #    profit_loss = (average_price - last_traded_price) * quantity
-        #else:
-        #    profit_loss = (average_price - last_traded_price) * quantity
+        if position['type'] == 'Buy':
+            profit_loss = (last_traded_price - average_price) * quantity
+        elif position['type'] == 'Sell':
+             profit_loss = (average_price - last_traded_price) * quantity
+        else:
+            profit_loss = (average_price - last_traded_price) * quantity
 
         # Calculate change percentage
         if average_price != 0:
@@ -169,12 +191,13 @@ def get_last_traded_price_and_profit_loss():
             change_percentage = 0.0
 
         # Update the position details with profit/loss and change percentage
-        # position['profit_loss'] = p_n_l
+        #position['profit_loss'] = profit_loss
         position['change_percentage'] = change_percentage
 
         data = {
             "last_traded_price": last_traded_price,
             "profit_loss": profit_loss,
+            "quantity": quantity,
             "change_percentage": change_percentage
         }
         return jsonify(data)
@@ -187,7 +210,7 @@ def place_buy_order():
 
     stock_symbol = request.form['stockSymbolBuy']
     quantity = int(request.form['quantity'])
-  
+
     # Define order details for a market buy order
     order_details = {
         "tradingsymbol": stock_symbol,
@@ -200,9 +223,10 @@ def place_buy_order():
 
     try:
         last_traded_price = get_last_traded_price(stock_symbol)
-        profit_loss = get_p_n_l
         order_id = kite.place_order(variety=kite.VARIETY_REGULAR, **order_details)
         trade_details = kite.order_trades(order_id)  # Fetch trade details
+        profit_loss = p_n_l
+        quantity = quantity
 
         if trade_details:
             average_price = trade_details[0]['average_price']
@@ -237,7 +261,7 @@ def place_buy_order():
 
                         if existing_position:
                             # Update the existing position with the new buy details
-                            existing_position['quantity'] += quantity
+                            #existing_position['quantity'] += quantity
                             existing_position['average_price'] = position['average_price']
                         else:
                             # Create a new position for the stock if it doesn't exist
@@ -285,17 +309,18 @@ def place_sell_order():
 
     try:
         last_traded_price = get_last_traded_price(stock_symbol)
-        profit_loss = get_p_n_l
         order_id = kite.place_order(variety=kite.VARIETY_REGULAR, **order_details)
         trade_details = kite.order_trades(order_id)  # Fetch trade details
+        profit_loss = p_n_l
+        quantity = quantity
 
         if trade_details:
             average_price = trade_details[0]['average_price']
 
             if last_traded_price is not None:
                 if order_details['transaction_type'] == 'SELL':
-                    #profit_loss = (average_price - last_traded_price) * quantity
-                    
+                    profit_loss = (average_price - last_traded_price) * quantity
+
                     if average_price != 0:
                         change_percentage = ((average_price - last_traded_price) / average_price) * 100
                     else:
@@ -315,7 +340,7 @@ def place_sell_order():
 
                 if existing_position:
                     # Update the existing position with the new sell details
-                    existing_position['quantity'] -= quantity
+                    #existing_position['quantity'] -= quantity
                     existing_position['average_price'] = average_price
                     existing_position['profit_loss'] = profit_loss
                     existing_position['change_percentage'] = change_percentage
@@ -326,10 +351,10 @@ def place_sell_order():
                         'product': order_details['product'],
                         'type': 'Sell',
                         'symbol': stock_symbol,
-                        'quantity': -quantity,  # Indicate a sell with negative quantity
+                        'quantity': quantity,  # Indicate a sell with negative quantity
                         'average_price': average_price,
                         'last_traded_price': last_traded_price,
-                        'profit_loss': profit_loss
+                        'profit_loss': p_n_l,
                         'change_percentage': change_percentage
                     })
                 return render_template('trade.html', order_confirmation=f"Sell order placed successfully. Order ID: {order_id}")
@@ -366,7 +391,7 @@ def logout():
 def run_app(port):
     app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     start_port = 8000
     end_port = 8005
 
